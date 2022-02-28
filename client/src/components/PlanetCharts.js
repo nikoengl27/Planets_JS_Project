e09c182e6e379bf78440c4a5937383f467003940
calumrodger <94742384+calumrodger@users.noreply.github.com>
2022-02-28 11:02:59 +0000

    working on highcharts

--- client/src/components/PlanetCharts.js
+++ client/src/components/PlanetCharts.js
@@ -3,15 +3,14 @@ import {render} from 'react-dom';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 
+
+
 const options = {
     chart: {
         type: 'column'
     },
     title: {
-        text: 'Monthly Average Rainfall'
-    },
-    subtitle: {
-        text: 'Source: WorldClimate.com'
+        text: 'Masses of the Planets'
     },
     xAxis: {
         min: 0,
@@ -40,47 +39,29 @@ const options = {
         }
     },
     series: [{
-        name: 'Tokyo',
         data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
-
-    }, {
-        name: 'New York',
-        data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
-
-    }, {
-        name: 'London',
-        data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
-
-    }, {
-        name: 'Berlin',
-        data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
 
     }]
 };
 
-// const options = {
-//     chart: {
-//       type: 'column'
-//     },
-//     title: {
-//       text: 'Masses of the Planets'
-//     },
-//     series: [
-//       {
-//         data: [1, 2, 1, 4, 3, 6]
-//       }
-//     ]
-//   };
+// const planetItems = ({planets}) => {
+//     planets.filter(checkIsPlanet);
+//     console.log(planetItems)
+// }
+
+// function checkIsPlanet(planet) {
+//     return planet.isPlanet === true;
+// }
 
 
-
-  const PlanetCharts = () => (
+const PlanetCharts = () => (
 
     <div>
-      <HighchartsReact highcharts={Highcharts} chart={chart} />
+      <HighchartsReact highcharts={Highcharts} options={options} />
     </div>
   );
-
+ planetItems()
   render(<PlanetCharts/>, document.getElementById('root'));
-
+  
 export default PlanetCharts
+  
