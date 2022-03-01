f1002508c4f3a6880a26c6c43492bf39a4ec568b
calumrodger <94742384+calumrodger@users.noreply.github.com>
2022-03-01 21:54:03 +0000

    attempted to add second question

--- client/src/components/PlanetQuiz.js
+++ client/src/components/PlanetQuiz.js
@@ -5,28 +5,26 @@ const PlanetQuiz = ({planets}) => {
 
 const [showAnswers, setShowAnswers] = useState(false)
 const [question1Planet, setQuestion1Planet] = useState([])
+const [question2Planet, setQuestion2Planet] = useState([])
 const [answer1, setAnswer1] = useState('')
-const [successState, setSuccessState] = useState(false)
+const [answer2, setAnswer2] = useState('')
+const [successState1, setSuccessState1] = useState(false)
+const [successState2, setSuccessState2] = useState(false)
+const [successStateOverall, setSuccessStateOverall] = useState(false)
 
 const isPlanetList = planets.filter((planet) => planet.isPlanet === true)
-const planetList = isPlanetList.map((planet) => ({name: planet.englishName, mass: planet.mass.massExponent, distance: planet.distanceFromTheSun, gravity: planet.gravity, volume: planet.vol.volExponent}))
+const planetList = isPlanetList.map((planet) => ({name: planet.englishName, mass: planet.mass.massExponent, distance: planet.distanceFromTheSun, gravity: planet.gravity, volume: planet.vol.volExponent, namesake: planet.namesake}))
 const planetNames = planetList.map((planet) => planet.name)
-const randomPlanet = planetList[Math.floor(Math.random() * planetList.length)];
-let correct
-// const randomPlanetName = question1Planet.name
-// const randomPlanetMass = question1Planet.mass
+const randomPlanet1 = planetList[Math.floor(Math.random() * planetList.length)];
+const randomPlanet2 = planetList[Math.floor(Math.random() * planetList.length)];
 
-// var objectKeys = Object.keys(planetList);
-// const randomProperty = planetList[objectKeys[ objectKeys.length * Math.random() << 0]];
-// console.log(randomProperty)
-// console.log(objectKeys)
 
 const question1 = `Which planet has a mass of ${question1Planet.mass}?`
-// console.log(Object.values(planetList)[Math.floor(Math.random()*Object.keys(planetList).length)]);
-// console.log(Object.getPropertyNames(planetList))
+const question2 = `Which planet has a namesake of ${question2Planet.namesake}`
 
 useEffect(() => {
     getQuestion1Planet();
+    getQuestion2Planet()
   }, [])
 
 console.log(question1Planet.name)
@@ -41,27 +39,54 @@ const handleSubmit = (event) => {
     const payload = answer1
     setAnswer1(payload)
     console.log(payload)
-    result(question1Planet, answer1)
+    resultOne(question1Planet, answer1)
+    resultTwo(question2Planet, answer2)
+    finalResult()
     submitQuiz()
   }
 
-const handleAnswerChange = (event) => {
+const handleAnswerChange1 = (event) => {
     setAnswer1(event.target.value)
 }
 
-const getQuestion1Planet = () => {
-    setQuestion1Planet(randomPlanet)
+const handleAnswerChange2 = (event) => {
+    setAnswer2(event.target.value)
 }
 
-const result = (question1Planet, answer1) => {
+const getQuestion1Planet = () => {
+    setQuestion1Planet(randomPlanet1)
+}
+
+const getQuestion2Planet = () => {
+    setQuestion2Planet(randomPlanet2)
+}
+
+const resultOne = (question1Planet, answer1) => {
     if(question1Planet.name === answer1){
-        setSuccessState(true) 
+        setSuccessState1(true) 
     } else {
-        setSuccessState(false)
+        setSuccessState1(false)
     }
 }
-console.log(successState)
 
+const resultTwo = (question2Planet, answer2) => {
+    if(question2Planet.name === answer2){
+        setSuccessState2(true) 
+    } else {
+        setSuccessState2(false)
+    }
+}
+
+const finalResult = (successState1, successState2) => {
+    if(successState1 === true || successState2 === true) {
+        setSuccessStateOverall(true)
+    } else {
+        setSuccessState2(false)
+    }
+}
+
+console.log(answer1)
+console.log(answer2)
 
 
 
@@ -70,19 +95,41 @@ return(
     { showAnswers ? null : 
     <div className="quiz">
     <form onSubmit={handleSubmit}>
-        <p>Hello {question1} </p>
-      <select name="first-planet" value={answer1} onChange={handleAnswerChange}>
+
+        <p>{question1} </p>
+      <select name="first-planet" value={answer1} onChange={handleAnswerChange1}>
           <option disabled>Choose...</option>
   {planetNames.map((name) => (<option>{name}</option>))}
     </select>
+
+        <p>{question2} </p>
+      <select name="second-planet" value={answer2} onChange={handleAnswerChange2}>
+          <option disabled>Choose...</option>
+  {planetNames.map((name) => (<option>{name}</option>))}
+    </select>
+
     <button onClick={submitQuiz}> Submit Answers</button>
     </form>
     </div>}
+
     { showAnswers ?
-    <div className="answers"> { successState ?
+    <div className="answers"> 
+
+    { successState1 ?
     <p>Question One Correct! You are correct in saying it is {answer1}.</p> :
     <p>Question One Very Very Wrong! The correct answer is {question1Planet.name} </p>}
-    </div> : null}
+
+    { successState2 ?
+    <p>Question Two Correct! You are correct in saying it is {answer2}.</p> :
+    <p>Question Two Very Very Wrong! The correct answer is {question2Planet.name} </p>}
+
+    {successStateOverall ?
+    <p> Well done you got them all!</p> :
+    <p> Bad luck you didn't get them all. </p>}
+
+
+    </div> 
+    : null}
     </>
 )
 
